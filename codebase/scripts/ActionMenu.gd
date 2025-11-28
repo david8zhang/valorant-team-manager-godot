36d1d027fe9cf9f3adb712c63dad47c30f786351
david8zhang <david8zhang@berkeley.edu>
2025-11-28 12:38:49 -0800

    tweak some UI behavior

--- codebase/scripts/ActionMenu.gd
+++ codebase/scripts/ActionMenu.gd
@@ -4,6 +4,10 @@ extends PanelContainer
 @onready var watch_button = $VBoxContainer/NonCombatAndStats/NonCombatActions/Watch as Button
 @onready var bomb_button = $VBoxContainer/NonCombatAndStats/NonCombatActions/Bomb as Button
 @onready var move_button = $VBoxContainer/NonCombatAndStats/NonCombatActions/Move as Button
+@onready var primary_weapon = $VBoxContainer/CombatAbilities/PrimaryWeapon as Button
+@onready var secondary_weapon = $VBoxContainer/CombatAbilities/SideArm as Button
+@onready var primary_weapon_ammo_meter = $VBoxContainer/CombatAbilities/PrimaryWeapon/ProgressBar as ProgressBar
+@onready var secondary_weapon_ammo_meter = $VBoxContainer/CombatAbilities/SideArm/ProgressBar as ProgressBar
 
 signal on_action(action_state)
 
@@ -11,13 +15,15 @@ func _ready() -> void:
 	move_button.pressed.connect(func (): on_action_click(AgentController.ActionState.MOVE))
 	bomb_button.pressed.connect(func (): on_action_click(AgentController.ActionState.DEFUSE))
 	watch_button.pressed.connect(func (): on_action_click(AgentController.ActionState.WATCH))
+	primary_weapon.pressed.connect(func (): on_action_click(AgentController.ActionState.PRIMARY_ATTACK))
+	secondary_weapon.pressed.connect(func (): on_action_click(AgentController.ActionState.SECONDARY_ATTACK))
 
 func on_action_click(action_state: AgentController.ActionState):
 	on_action.emit(action_state)
 	set_button_highlight_from_state(action_state)
 
 func set_button_highlight_from_state(curr_action_state: AgentController.ActionState):
-	dehighlight_all_buttons()
+	de_highlight_all_buttons()
 	match curr_action_state:
 		AgentController.ActionState.MOVE:
 			highlight_single_button(move_button)
@@ -25,18 +31,24 @@ func set_button_highlight_from_state(curr_action_state: AgentController.ActionSt
 			highlight_single_button(watch_button)
 		AgentController.ActionState.DEFUSE, AgentController.ActionState.PLANT:
 			highlight_single_button(bomb_button)
+		AgentController.ActionState.PRIMARY_ATTACK:
+			highlight_single_button(primary_weapon)
+		AgentController.ActionState.SECONDARY_ATTACK:
+			highlight_single_button(secondary_weapon)
 
-func dehighlight_all_buttons():
+func de_highlight_all_buttons():
 	dehighlight_single_button(watch_button)
 	dehighlight_single_button(bomb_button)
 	dehighlight_single_button(move_button)
+	dehighlight_single_button(primary_weapon, 0.75)
+	dehighlight_single_button(secondary_weapon, 0.75)
 
-func dehighlight_single_button(button):
+func dehighlight_single_button(button, color = 0.6):
 	var sb := StyleBoxFlat.new()
-	sb.bg_color = Color(0.6, 0.6, 0.6)  # blue background
+	sb.bg_color = Color(color, color, color)
 	button.add_theme_stylebox_override("normal", sb)
 
 func highlight_single_button(button):
 	var sb := StyleBoxFlat.new()
-	sb.bg_color = Color(0, 1, 0)  # blue background
+	sb.bg_color = Color(0, 1, 0)
 	button.add_theme_stylebox_override("normal", sb)
--- codebase/scripts/AgentController.gd
+++ codebase/scripts/AgentController.gd
@@ -21,16 +21,25 @@ signal on_complete_turn
 
 var selected_agent: Agent
 var curr_action_state: ActionState = ActionState.NONE
+var is_hovering_action_menu := false
 
 func _ready() -> void:
 	await game_round.ready
 	game_round.action_menu.on_action.connect(set_action_state_move)
+	game_round.action_menu.mouse_entered.connect(enter_hover_action_menu)
+	game_round.action_menu.mouse_exited.connect(exit_hover_action_menu)
 
 func set_action_state_move(new_action_state: ActionState):
 	curr_action_state = new_action_state
 
+func exit_hover_action_menu():
+	is_hovering_action_menu = false
+
+func enter_hover_action_menu():
+	is_hovering_action_menu = true
+
 func _process(_delta):
-	if game_round.curr_turn_side == GameRound.Side.PLAYER:
+	if game_round.curr_turn_side == GameRound.Side.PLAYER and !is_hovering_action_menu: 
 		if Input.is_action_just_pressed("mouse_left") and curr_action_state == ActionState.MOVE:
 			var pos_to_move_to = game_round.map.ground_layer.map_to_local(highlight_overlay.hovered_tile_pos)
 			selected_agent.move_to_position(pos_to_move_to, complete_move)
