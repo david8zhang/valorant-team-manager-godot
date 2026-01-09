2788355b775a692ab0d76eb0d10cfd05697e1e35
david8zhang <david8zhang@berkeley.edu>
2026-01-08 18:03:14 -0800

    added abilities, armor section to buy menu

---
+++ codebase/scripts/AgentBuyMenu.gd
@@ -0,0 +1,7 @@
+class_name AgentBuyMenu
+extends Control
+
+@onready var agent_name_label = $PanelContainer/VBoxContainer/PanelContainer/MarginContainer2/HBoxContainer/Label
+@onready var kill_count_label = $PanelContainer/VBoxContainer/PanelContainer/MarginContainer2/HBoxContainer/HBoxContainer/KillCount
+@onready var death_count_label = $PanelContainer/VBoxContainer/PanelContainer/MarginContainer2/HBoxContainer/HBoxContainer2/DeathCount
+@onready var assist_count_label = $PanelContainer/VBoxContainer/PanelContainer/MarginContainer2/HBoxContainer/HBoxContainer3/AssistCount
---
+++ codebase/scripts/BuyMenu.gd
@@ -0,0 +1,5 @@
+class_name BuyMenu
+extends Control
+
+@onready var scoreboard = $PanelContainer/VBoxContainer/Scoreboard as Scoreboard
+@export var agent_buy_menu_scene: PackedScene
