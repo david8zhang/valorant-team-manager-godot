0979fe652910f8b861d03ab8a07de7ecc2c07397
david8zhang <david8zhang@berkeley.edu>
2026-04-18 11:38:33 -0700

    update turn order cards with player info

--- codebase/scripts/GameRoundVariables.gd
+++ codebase/scripts/GameRoundVariables.gd
@@ -89,4 +89,4 @@ func purchase_ability_charge(agent_name, ability_index):
 		agent_game_stats.ability_2_charges += 1
 		
 func load_random_agent_stat():
-	return AGENT_STAT_RESOURCES[1]
+	return AGENT_STAT_RESOURCES.pick_random()
--- codebase/scripts/TurnOrderCard.gd
+++ codebase/scripts/TurnOrderCard.gd
@@ -7,6 +7,10 @@ extends Control
 
 func update_from_agent(agent: Agent):
 	agent_name_label.text = agent.agent_name
+	texture_rect.texture = agent.agent_stats.texture
+	var base := panel_container.get_theme_stylebox("panel") as StyleBoxFlat
+	base.bg_color = GameRoundVariables.CPU_OUTLINE_COLOR if agent.curr_side == GameRound.Side.CPU else GameRoundVariables.PLAYER_OUTLINE_COLOR
+	base.bg_color.a = 0.25
 
 func highlight_curr_card():
 	var base := panel_container.get_theme_stylebox("panel") as StyleBoxFlat
